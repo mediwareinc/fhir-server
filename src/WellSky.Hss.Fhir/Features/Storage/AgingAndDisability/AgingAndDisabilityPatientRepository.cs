16ddb63f2813bb3a5169fef39f39406a3b30e6df
Aldo Gutierrez Tanabe <aldo.gutierrez@globant.com>
2024-10-02 14:00:04 -0500

    Implement stub mapper

--- src/WellSky.Hss.Fhir/Features/Storage/AgingAndDisability/AgingAndDisabilityPatientRepository.cs
+++ src/WellSky.Hss.Fhir/Features/Storage/AgingAndDisability/AgingAndDisabilityPatientRepository.cs
@@ -1,28 +1,34 @@
-﻿using Microsoft.Health.Fhir.Core.Features.Persistence;
-using Microsoft.Health.Fhir.Core.Models;
-using WellSky.Hss.Fhir.Features.Storage.AgingAndDisability.InternalRepositories;
-
-namespace WellSky.Hss.Fhir.Features.Storage.AgingAndDisability
+﻿namespace WellSky.Hss.Fhir.Features.Storage.AgingAndDisability
 {
+    using Hl7.Fhir.Serialization;
+    using InternalRepositories;
+    using Mappers;
+    using Microsoft.Health.Fhir.Core.Features.Persistence;
+    using Microsoft.Health.Fhir.Core.Models;
+
     public class AgingAndDisabilityPatientRepository : IAgingAndDisabilityFhirRepository
     {
+        private readonly FhirJsonSerializer _fhirJsonSerializer;
         private readonly IConsumerRepository _consumerRepository;
 
-        public AgingAndDisabilityPatientRepository(IConsumerRepository consumerRepository)
+        public AgingAndDisabilityPatientRepository(FhirJsonSerializer fhirJsonSerializer, IConsumerRepository consumerRepository)
         {
+            _fhirJsonSerializer = fhirJsonSerializer;
             _consumerRepository = consumerRepository;
         }
 
         public async Task<ResourceWrapper> GetAsync(ResourceKey key, string deploymentId, CancellationToken cancellationToken)
         {
             var consumer = await _consumerRepository.GetAsync(deploymentId, Guid.Parse(key.Id));
+            var patient = ConsumerMapper.Map(consumer);
+            var resourceJson = await _fhirJsonSerializer.SerializeToStringAsync(patient);
 
             // TODO Aldo: Research how to map ResourceWrapper, do we have everything we need from DB?
             return new ResourceWrapper(
                 key.Id, // TODO Aldo: Should use ReferenceId convention from here?
                 "1", // TODO Aldo: how are we planning to get version?
                 KnownResourceTypes.Patient,
-                new RawResource("resource data as JSON here", FhirResourceFormat.Json, true),
+                new RawResource(resourceJson, FhirResourceFormat.Json, true),
                 new ResourceRequest("GET"),
                 DateTimeOffset.UtcNow,
                 false,
--- src/WellSky.Hss.Fhir/Features/Storage/AgingAndDisability/Mappers/ConsumerMapper.cs
+++ src/WellSky.Hss.Fhir/Features/Storage/AgingAndDisability/Mappers/ConsumerMapper.cs
@@ -5,20 +5,20 @@ namespace WellSky.Hss.Fhir.Features.Storage.AgingAndDisability.Mappers
 {
     internal class ConsumerMapper
     {
-        //public static Patient Map(Consumer consumer)
-        //{
-        //    // TODO Aldo: Map and return FHIR model from A&D data...
-        //    return new Patient
-        //    {
-        //        Id = consumer.Id.ToString(),
-        //        Name =
-        //        [
-        //            new HumanName
-        //            {
-        //                Given = [consumer.Client.FirstName]
-        //            }
-        //        ]
-        //    };
-        //}
+        public static Patient Map(Consumer consumer)
+        {
+            // TODO Aldo: Map and return FHIR model from A&D data...
+            return new Patient
+            {
+                Id = consumer.Id.ToString(),
+                Name =
+                [
+                    new HumanName
+                    {
+                        Given = ["test name"]
+                    }
+                ]
+            };
+        }
     }
 }
