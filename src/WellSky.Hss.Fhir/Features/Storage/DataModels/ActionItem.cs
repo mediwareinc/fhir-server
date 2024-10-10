d75c987942a1ad6344411388adf9c9ee42650e7e
Aldo Gutierrez Tanabe <aldo.gutierrez@globant.com>
2024-10-10 11:25:20 -0500

    Add minimal A&D models

---
+++ src/WellSky.Hss.Fhir/Features/Storage/DataModels/ActionItem.cs
@@ -0,0 +1,12 @@
+﻿namespace WellSky.Hss.Fhir.Features.Storage.DataModels
+{
+    using System.Diagnostics.CodeAnalysis;
+
+    [ExcludeFromCodeCoverage]
+    public class ActionItem : BaseModel, IAgingAndDisabilityModel
+    {
+        public Guid Id { get; set; }
+
+        // TODO Kayla: add required properties
+    }
+}
